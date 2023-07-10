9288d3528d459567f3500e2279b500c97308ba67
rbatorsky <rebecca.batorsky@tufts.edu>
2023-07-10 14:25:32 -0400

    add scripts

---
+++ scripts/01_seurat_object_quality_control.R
@@ -0,0 +1,251 @@
+# This script reads in the seurat object, checks normalization and makes summary HC and PCA plots
+
+suppressPackageStartupMessages({library(org.Hs.eg.db)
+  library(tidyverse)
+  library(Seurat)
+  library(clusterProfiler)
+  library(Libra)
+  library(DESeq2)
+  library(magrittr)
+  library(ggrepel)
+  library("pheatmap")
+  library("RColorBrewer")
+  })
+
+# Load integrated dataset from Britt ----
+so_file="/cluster/tufts/slonimlab/rbator01/human_scrna_edlow_2021/seurat_object/seuratobj_subset_integrated_HBCs_USETHISONE.rds"
+so = readRDS(so_file)
+res="sub_cluster_names"
+Idents(so) = res
+
+# Since these are equal, this has not been normalized
+all(so[['RNA']]@counts@x == so[['RNA']]@data@x)
+
+# Normalize
+DefaultAssay(so) <- "RNA"
+so = NormalizeData(so)
+
+# Rename the clusters 
+# convert the cluster names
+names = so@meta.data %>% 
+  dplyr::select(c('integrated_snn_res.0.3','sub_cluster_names')) %>% 
+  mutate(cluster = as.character(integrated_snn_res.0.3)) 
+
+rownames(names) = rownames(so@meta.data)
+
+names$sub_cluster_names = gsub(pattern = "HBCs_", replacement = "HBC ",x = names$sub_cluster_names)
+names$sub_cluster_names = gsub(pattern = "Monocytes", replacement = "Monocyte",x = names$sub_cluster_names)
+names$sub_cluster_names = gsub(pattern = "PAMs", replacement = "PAMM",x = names$sub_cluster_names)
+
+so = AddMetaData(so,
+                 names$sub_cluster_names,
+                 "sub_cluster_names")
+
+saveRDS(so, "/cluster/tufts//slonimlab/rbator01/human_scrna_edlow_2021/seurat_object/seuratobj_subset_integrated_HBCs_USETHISONE_norm.rds")
+
+# PCA for quality control ----
+
+# assign one cluster to do full sample pseudobulk
+so@meta.data$one_cluster = "one"
+
+unique(so@meta.data$donor)
+unique(so@meta.data$covid_status)
+
+level1='negative'
+level2='recovered'
+
+metadata <- so@meta.data
+metadata$donor= as.factor(metadata$donor)
+metadata$covid_status = as.factor(metadata$covid_status)
+metadata$sub_cluster_names = as.factor(metadata$sub_cluster_names)
+so@meta.data = metadata
+
+min_cells = 0
+min_reps = 0
+cell_type_col="one_cluster"
+replicate_col="donor"
+label_col="covid_status"
+level1=level1
+level2=level2
+
+pseudobulks_orig_ident=Libra::to_pseudobulk(so, 
+                                            min_cells = min_cells,
+                                            min_reps = min_reps, 
+                                            cell_type_col=cell_type_col,
+                                            replicate_col=replicate_col,
+                                            label_col=label_col)
+
+rownames(metadata) = NULL
+deseq_meta = unique(so@meta.data[c("donor", "sex","covid_status","batch")])
+deseq_meta
+rownames(deseq_meta) = deseq_meta$donor
+
+deseq_counts = pseudobulks_orig_ident$one
+colnames(deseq_counts)
+
+colnames = data.frame(names=colnames(deseq_counts))
+colnames = colnames %>% separate(names, into=c("name","status"), sep=":")
+colnames(deseq_counts) = colnames$name
+deseq_counts <- deseq_counts[, rownames(deseq_meta)]
+colnames(deseq_counts)
+
+# check order
+all(colnames(deseq_counts) %in% rownames(deseq_meta))
+all(colnames(deseq_counts) == rownames(deseq_meta))
+
+## run deseq analysis
+#design = model.matrix(~ group, data = targets)
+
+dds = DESeqDataSetFromMatrix(countData = deseq_counts,
+                             colData = deseq_meta,
+                             design = ~ sex)
+
+
+dds = DESeq(dds)
+
+# Plotting a gene
+d <- plotCounts(dds, gene="CCL8", intgroup="sex", returnData=TRUE)
+
+ggplot(d, aes(x = sex, y = count, color = sex)) +
+  geom_point(position=position_jitter(w = 0.1,h = 0)) +
+  geom_text_repel(aes(label = rownames(d))) +
+  theme_bw() +
+  ggtitle("Hnrnpm") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+res = results(dds)
+res.df = data.frame(res)
+res.df$gene = rownames(res.df)
+res.df %>% dplyr::filter(gene == "CCL8")
+
+# check the PCA
+vsd <- DESeq2::vst(dds)
+rv=rowVars(assay(vsd))
+select <- order(rv, decreasing=TRUE)[seq_len(min(100, length(rv)))]
+pc = prcomp(t(assay(vsd)[select,]))
+loadings = as.data.frame(pc$rotation)
+aload = abs(loadings)
+sweep(aload, 2, colSums(aload), "/")
+aload$gene = rownames(aload)
+ 
+aload_long <- aload %>% 
+  gather(pc, value, -gene) %>%
+  group_by(pc) %>%
+  top_n(20, wt=value)
+
+df <- as.data.frame(colData(dds)[,c("donor","sex","covid_status", "batch")])
+
+
+annoCol<-list(sex=c(M="blue", F="red"),
+              covid_status=c(negative="orange", recovered="green"),
+              batch=c(AEb14="blue", AEb13 = "yellow", AEb12="grey", AEb11="green",AEb9 = "orange",
+                      AEb8 = "black", AEb7 = "purple"))
+
+
+pheatmap(assay(vsd)[select,], 
+         cluster_rows=T, 
+         show_rownames=T,
+         cluster_cols=T, 
+         annotation_col=df, 
+         annotation_colors = annoCol)
+
+vsd_cor <- cor(assay(vsd))
+pheatmap(vsd_cor, 
+         cluster_rows=T, 
+         show_rownames=T,
+         cluster_cols=T, 
+         annotation_col=df, 
+         annotation_colors = annoCol)
+
+# plotPCA ----
+vsd <- DESeq2::vst(dds)
+rv=rowVars(assay(vsd))
+intgroup=c("donor","covid_status")
+pcaData <- plotPCA(vsd, intgroup=intgroup, returnData=TRUE)
+ntop=10
+select <- order(rv, decreasing = TRUE)[seq_len(min(ntop,length(rv)))]
+pca <- prcomp(t(assay(vsd)[select, ]))
+percentVar <- pca$sdev^2/sum(pca$sdev^2)
+
+intgroup.df <- as.data.frame(colData(vsd)[, intgroup, drop = FALSE])
+
+group <- if (length(intgroup) > 1) {
+  factor(apply(intgroup.df, 1, paste, collapse = " : "))
+}else{
+  colData(object)[[intgroup]]
+}
+
+d <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], group = group, 
+                intgroup.df, name = colData(vsd)[,1])
+
+
+attr(d, "percentVar") <- percentVar[1:2]
+
+
+pcaData <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], PC3 = pca$x[, 3], PC4 = pca$x[, 4], 
+                      group = group, intgroup.df, name = colnames(vsd))
+
+
+head(pcaData)
+percentVar <- round(100 * percentVar)
+
+ggplot(pcaData, aes(PC1, PC2, color=covid_status,label=donor)) +
+  geom_point(size=3) +
+  geom_text(hjust=0.2, vjust=0.2) + 
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  coord_fixed()
+
+
+ggplot(pcaData, aes(PC3, PC4, color=covid_status,label=donor)) +
+  geom_point(size=3) +
+  geom_text(hjust=0.2, vjust=0.2) + 
+  xlab(paste0("PC3: ",percentVar[3],"% variance")) +
+  ylab(paste0("PC4: ",percentVar[4],"% variance")) + 
+  coord_fixed()
+
+
+# QC of  DEG results - move to another file ---- 
+
+## No mincell
+no_mincell_fixbh_results = read.csv("/cluster/tufts/slonimlab/rbator01/human_scrna_edlow_2021/deg/deg_rec_vs_neg_lfcthersh0_seuratobj_subset_integrated_HBCs_USETHISONE_fixbh.csv")
+head(no_mincell_fixbh_results)
+
+# rawp
+sig_results_rawp = no_mincell_fixbh_results %>% dplyr::filter(p_val < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_rawp$cluster)
+
+# bhp
+sig_results_fix_bh = no_mincell_fixbh_results %>% dplyr::filter(p_val_adj_bh < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_fix_bh$cluster)
+#write.csv(sig_results_fix_bh, "~/slonimlab/rbator01/human_scrna_edlow_2021/deg/deg_rec_vs_neg_lfcthersh0_seuratobj_subset_integrated_HBCs_USETHISONE_bh_0.05_lfc_0.2_fixbh.csv")
+
+# bonp
+sig_results_bon = no_mincell_fixbh_results %>% dplyr::filter(p_val_adj < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_bon$cluster)
+
+
+## Yes Mincell
+fix_bh_results = read.csv("~/slonimlab/rbator01/human_scrna_edlow_2021/deg/deg_rec_vs_neg_lfcthersh0_seuratobj_subset_integrated_HBCs_USETHISONE_mincell_fixbh.csv")
+
+#rawp
+sig_results_rawp = fix_bh_results %>% dplyr::filter(p_val < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_rawp$cluster)
+
+# bhp
+sig_results_fix_bh = fix_bh_results %>% dplyr::filter(p_val_adj_bh < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_fix_bh$cluster)
+
+#bonp
+sig_results_bon = fix_bh_results %>% dplyr::filter(p_val_adj < 0.05 & abs(avg_log2FC) > 0.2) %>% arrange(cluster)
+table(sig_results_bon$cluster)
+ 
+
+## Checking some of Britt's markers
+# wilcox_markers=read.csv("~/slonimlab/rbator01/human_scrna_edlow_2021/markers.wilcox_seuratobj_subset_integrated_HBCs.csv",row.names=NULL)
+# negbin_markers=read.csv("~/slonimlab/rbator01/human_scrna_edlow_2021/markers.negbin_seuratobj_subset_integrated_HBCs.csv")
+# wilcox_markers_prenorm=read.csv("~/slonimlab/rbator01/human_scrna_edlow_2021/before_norm/markers.wilcox_seuratobj_subset_integrated_HBCs.csv")
+# negbin_markers_prenorm=read.csv("~/slonimlab/rbator01/human_scrna_edlow_2021/before_norm/markers.negbin_seuratobj_subset_integrated_HBCs.csv")
+# britt_wilcox=read.csv("/cluster/home/rbator01/slonimlab/rbator01/human_scrna_edlow_2021/markers_res03_integrated_wilcox.csv")
+# britt_negbin=read.csv("/cluster/home/rbator01/slonimlab/rbator01/human_scrna_edlow_2021/markers_res03_integrated_negbinom.csv")
---
+++ scripts/02_correlation_with_reference_placenta_humandata.R
@@ -0,0 +1,208 @@
+# This script outputs plots of the correlation with reference placenta clusters
+# code adapted from https://github.com/archavan/covid-placenta
+
+suppressPackageStartupMessages({library(org.Hs.eg.db)
+  library(tidyverse)
+  library(Seurat)
+  library(clusterProfiler)
+  library(org.Hs.eg.db)
+  library(ComplexHeatmap)
+  
+})
+
+#  load data ----
+so_file="/cluster/tufts/slonimlab/rbator01/human_scrna_edlow_2021/seurat_object/seuratobj_subset_integrated_HBCs_USETHISONE_norm.rds"
+so = readRDS(so_file)
+Idents(so) = "sub_cluster_names"
+DefaultAssay(so) <- "RNA"
+
+# Reference datasets ----
+ref_file="/cluster/tufts/slonimlab/rbator01/reference_data/three_dataset_merged_markers/vento-surya-covid_control-joined.csv"
+ref <- read.csv(ref_file, stringsAsFactors = FALSE)
+
+# rename
+names(ref) <- gsub("lu", "l", names(ref))
+names(ref) <- gsub("surya", "s", names(ref))
+names(ref) <- gsub("vento", "v", names(ref))
+
+# Correlation matrix
+so.avg <- AverageExpression(so)
+so.avg.rna <- so.avg$RNA
+so.avg.rna = data.frame(so.avg.rna)
+colnames(so.avg.rna) = gsub("^","P_",colnames(so.avg.rna))
+so.avg.rna$gene_name <- rownames(so.avg.rna)
+
+## join reference and placenta
+plac_ref=dplyr::inner_join(so.avg.rna, ref, by = "gene_name")
+plac_ref$HGNC.symbol = NULL
+
+# Annotation by correlation ----
+
+refdata <- c("l","s","v")
+
+# build correlation matrix from expression data
+cor.matrix <- cor(plac_ref[, names(plac_ref)[names(plac_ref) != "gene_name" ]], 
+                  method = 'spearman')
+
+# reorder correlation matrix based on clustering
+dd <- as.dist((1 - cor.matrix))
+hc <- hclust(dd, method = 'complete')
+cor.matrix <- cor.matrix[hc$order, hc$order]
+
+# melt correlation matrix
+cormat <- reshape2::melt(cor.matrix, na.rm = T)
+cormat$Var1_source <- sapply(strsplit(as.character(cormat$Var1), split = "_"), "[[", 1)
+cormat$Var2_source <- sapply(strsplit(as.character(cormat$Var2), split = "_"), "[[", 1)
+
+# subset
+cormat <- cormat[which(cormat$Var1_source %in% refdata & 
+                         cormat$Var2_source %in% c("P")), ]
+
+# top 3 matches with highest correlation coefficients
+topmatch <- data.frame( # empty df to fill top matches from the loop below
+  cluster = unique(as.character(cormat$Var2)) %>% sort(),
+  vento.top1 = NA,
+  surya.top1 = NA,
+  covid.top1 = NA,
+  vento.top2 = NA,
+  surya.top2 = NA,
+  covid.top2 = NA,
+  vento.top3 = NA,
+  surya.top3 = NA,
+  covid.top3 = NA
+)
+
+cormat$top3 <- NA
+
+for(i in unique(cormat$Var2)){
+  for(j in refdata){
+    # identify top3 match indices
+    ind <- which(cormat$Var2 == i & cormat$Var1_source == j)
+    val <- cormat$value[ind]
+    top3ind <- ind[order(val, decreasing = TRUE)[1:3]]
+    
+    # assign match ranking to correlation data for plotting
+    cormat$top3[top3ind[1]] <- "1"
+    cormat$top3[top3ind[2]] <- "2"
+    cormat$top3[top3ind[3]] <- "3"
+    
+    # assign top matches to topmatches dataframe
+    topmatch[topmatch$cluster == i, paste0(j, ".top1")] <- gsub(paste0(j, "_"), "", as.character(cormat$Var1[top3ind[1]]))
+    topmatch[topmatch$cluster == i, paste0(j, ".top2")] <- gsub(paste0(j, "_"), "", as.character(cormat$Var1[top3ind[2]]))
+    topmatch[topmatch$cluster == i, paste0(j, ".top3")] <- gsub(paste0(j, "_"), "", as.character(cormat$Var1[top3ind[3]]))
+  }
+}
+library(viridis)
+
+# Plot all, this is not the most informative plot, it is easier to see if you break down the individual datasets as below -----
+
+# output directory for plots
+outdir="/cluster/tufts/slonimlab/rbator01/human_scrna_edlow_2021/plots_compare_reference/"
+
+p <- ggplot(data = cormat,
+            aes(Var1, Var2, fill = value)) +
+  geom_tile(colour = "white") +
+  scale_fill_viridis(name = "Spearman\nCorrelation") +
+  geom_point(aes(Var1, Var2, alpha = top3),
+             size = 1.5, shape = 19, stroke  = 0) +
+  scale_alpha_manual(values = c(1, 0.5, 0.25),
+                     breaks = c(1, 2, 3),
+                     name = "top3\nwithin dataset", na.value = 0) +
+  coord_fixed(ratio = 1) +
+  xlab("reference") +
+  ylab("query") +
+  labs(caption = "For each celltype in query, black points represent top 3 celltypes from reference with highest correlation.",
+       title = paste0("pl vs.reference")) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, size = 8,
+                                   hjust=1, vjust = 0.5),
+        axis.text.y = element_text(size=8),
+        axis.ticks.length = unit(0.15, units = c('lines')),
+        legend.title = element_text(size = 10),
+        axis.title = element_text(size = 8),
+        plot.caption = element_text(size = 7),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        legend.key.size = unit(0.8, units = c('lines')))
+
+show(p)
+ggsave(p, filename = paste0(outdir, "~/analysis/plots/ref_compare/pl_subset_clusters_res_0.2_vs_reference_corrplot_clusters.pdf"),
+       device = "pdf", width = 15, height = 7, units = "in")
+
+
+# plotting function
+clustAnnoPlot <- function(dat, query, reference, plot_title) {
+  
+  dat <- dat[which(dat$Var2_source %in% query & dat$Var1_source == reference), ]
+
+  p <- ggplot(data = dat, 
+              aes(Var1, Var2, fill = value)) +
+    geom_tile(colour = "white") +
+    scale_fill_viridis(name = "Spearman\nCorrelation") +
+    geom_point(aes(Var1, Var2, alpha = top3),
+               size = 1.5, shape = 19, stroke  = 0) +
+    scale_alpha_manual(values = c(1, 0.5, 0.25), 
+                       breaks = c(1, 2, 3),
+                       name = "top3", na.value = 0) +
+    coord_fixed(ratio = 1) +
+    xlab("reference") +
+    ylab("query") +
+    
+    labs(title = plot_title) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, size = 8, 
+                                     hjust=1, vjust = 0.5),
+          axis.text.y = element_text(size=8),
+          axis.ticks.length = unit(0.15, units = c('lines')),
+          legend.title = element_text(size = 10),
+          axis.title = element_text(size = 8),
+          plot.caption = element_text(size = 7),
+          panel.grid = element_blank(),
+          panel.border = element_blank(),
+          legend.key.size = unit(0.8, units = c('lines')))
+  
+  return(p)
+}
+
+clustAnnoPlot_notop <- function(dat, query, reference, plot_title) {
+  
+  dat <- dat[which(dat$Var2_source %in% query & dat$Var1_source == reference), ]
+  
+  p <- ggplot(data = dat, 
+              aes(Var1, Var2, fill = value)) +
+    geom_tile(colour = "white") +
+    scale_fill_viridis(name = "Spearman\nCorrelation") +
+    coord_fixed(ratio = 1) +
+    xlab("reference") +
+    ylab("query") +
+    labs(title = plot_title) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, size = 8, 
+                                     hjust=1, vjust = 0.5),
+          axis.text.y = element_text(size=8),
+          axis.ticks.length = unit(0.15, units = c('lines')),
+          legend.title = element_text(size = 10),
+          axis.title = element_text(size = 8),
+          plot.caption = element_text(size = 7),
+          panel.grid = element_blank(),
+          panel.border = element_blank(),
+          legend.key.size = unit(0.8, units = c('lines')))
+  
+  return(p)
+}
+
+### Against Vento-Tormo et al
+anno.vento <- clustAnnoPlot(dat = cormat, query = "P", reference = "v", plot_title="Vento-Tormo et al")
+cowplot::ggsave2(anno.vento, device = "pdf", width = 8, height = 4, units = "in",
+                 filename = paste0(outdir, "vento_subset_heatmap.pdf"))
+
+## Against "Lu-Culligan et al
+anno.covid <- clustAnnoPlot(dat = cormat, query = c("P"), reference = "l", plot_title="Lu-Culligan et al")
+cowplot::ggsave2(anno.covid, device = "pdf", width = 6, height = 4, units = "in",
+                 filename = paste0(outdir, "lu_c_subset_heatmap.pdf"))
+show(anno.covid)
+### Against Suryavanshi et al
+anno.surya <- clustAnnoPlot(dat = cormat, query = c("P"), reference = "s", plot_title="Suryawanshi et al")
+cowplot::ggsave2(anno.surya, device = "pdf", width = 6, height = 4, units = "in",
+                 filename = paste0(outdir, "surya_subset_heatmap.pdf"))
+show(anno.surya)
